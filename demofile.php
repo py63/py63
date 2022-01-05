17ae1dc543cbf57a581dcc3397ef4e9445981bd8
py63 <95627593+py63@users.noreply.github.com>
2022-01-05 15:56:59 +0530

    URL setup

--- demofile.php
+++ demofile.php
@@ -1,1 +1,6 @@
-<?php echo "textfile" ?>
+UPDATE blog_options SET option_value = replace(option_value, 'https://blog.galaxyweblinks.com', 'https://php72.gwl.com/bloggalaxy') WHERE option_name = 'home' OR option_name = 'siteurl';
+UPDATE blog_posts SET guid = replace(guid, 'https://blog.galaxyweblinks.com','https://php72.gwl.com/bloggalaxy');
+UPDATE blog_postmeta SET meta_value = replace(meta_value,'https://blog.galaxyweblinks.com','https://php72.gwl.com/bloggalaxy');
+UPDATE blog_posts SET post_content = replace(post_content, 'https://blog.galaxyweblinks.com', 'https://php72.gwl.com/bloggalaxy');
+
+update wp_users set user_email = concat('_', user_email, '_');
